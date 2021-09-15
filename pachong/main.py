eace635990eaa5175c5a303065c6e9ca57e8b71a
zhangyunqi <1196385490@qq.com>
2021-09-15 19:32:47 +0800

    获取复权因子

--- pachong/main.py
+++ pachong/main.py
@@ -133,9 +133,8 @@ def calculate_zhibiao(wangyi_stock_code: str):
     data = data.reindex(index=data.index[::-1])
     stock = StockDataFrame.retype(data)
     # sma
-
+    stock['close_5_sma'], stock['close_10_sma'], stock['close_30_sma'], stock['close_60_sma']
     # ema
-
     # boll:boll-中线  boll_ub:boll-up上线  boll_lb: boll下线
     stock['boll'], stock['boll_ub'], stock['boll_lb']
     # macd:DIF macds:DEA macdh*2:MACD
@@ -143,4 +142,77 @@ def calculate_zhibiao(wangyi_stock_code: str):
     # kdj
     stock['kdjk'], stock['kdjd'], stock['kdjj']
     # rsi
-    stock['rsi_6'],stock['rsi_12'],stock['rsi_24']
+    stock['rsi_6'], stock['rsi_12'], stock['rsi_24']
+
+
+# 计算指标
+def calculate_zhibiao1(wangyi_stock_code: str, fq_flag: int):
+    # 股票数据
+    stock_datas: pd.DataFrame = pd.read_csv(stock_file_path.format(wangyi_stock_code)
+                                            , encoding="GBK")
+    if fq_flag != 0:
+        # 复权处理
+        # 股票复权因子数据
+        stock_factors: pd.DataFrame = pd.read_csv(stock_factor_file_path.format(wangyi_stock_code)
+                                                  , encoding="GBK", index_col='trade_date')
+        if fq_flag == 1:
+            # 前复权
+            # 最新的复权因子
+            new_factor = stock_factors.iloc[0]['adj_factor']
+            stock_datas["收盘价"] = list(
+                map(lambda x, y: y * (stock_factors.loc[int(str.replace(x, '-', ''))]['adj_factor']) / new_factor,
+                    stock_datas["日期"], stock_datas["收盘价"]))
+            stock_datas["最高价"] = list(
+                map(lambda x, y: y * (stock_factors.loc[int(str.replace(x, '-', ''))]['adj_factor']) / new_factor,
+                    stock_datas["日期"], stock_datas["最高价"]))
+            stock_datas["最低价"] = list(
+                map(lambda x, y: y * (stock_factors.loc[int(str.replace(x, '-', ''))]['adj_factor']) / new_factor,
+                    stock_datas["日期"], stock_datas["最低价"]))
+            stock_datas["开盘价"] = list(
+                map(lambda x, y: y * (stock_factors.loc[int(str.replace(x, '-', ''))]['adj_factor']) / new_factor,
+                    stock_datas["日期"], stock_datas["开盘价"]))
+            stock_datas["前收盘"] = list(
+                map(lambda x, y: y * (stock_factors.loc[int(str.replace(x, '-', ''))]['adj_factor']) / new_factor,
+                    stock_datas["日期"], stock_datas["前收盘"]))
+        elif fq_flag == 2:
+            # 后复权
+            stock_datas["收盘价"] = list(
+                map(lambda x, y: y * stock_factors.loc[int(str.replace(x, '-', ''))]['adj_factor'],
+                    stock_datas["日期"], stock_datas["收盘价"]))
+            stock_datas["最高价"] = list(
+                map(lambda x, y: y * stock_factors.loc[int(str.replace(x, '-', ''))]['adj_factor'],
+                    stock_datas["日期"], stock_datas["最高价"]))
+            stock_datas["最低价"] = list(
+                map(lambda x, y: y * stock_factors.loc[int(str.replace(x, '-', ''))]['adj_factor'],
+                    stock_datas["日期"], stock_datas["最低价"]))
+            stock_datas["开盘价"] = list(
+                map(lambda x, y: y * stock_factors.loc[int(str.replace(x, '-', ''))]['adj_factor'],
+                    stock_datas["日期"], stock_datas["开盘价"]))
+            stock_datas["前收盘"] = list(
+                map(lambda x, y: y * stock_factors.loc[int(str.replace(x, '-', ''))]['adj_factor'],
+                    stock_datas["日期"], stock_datas["前收盘"]))
+
+    # 过滤停牌的交易日，条件"收盘价","最高价","最低价","开盘价","成交量"均为0
+    stock_datas = stock_datas[(stock_datas["收盘价"] != 0) & (stock_datas["最高价"] != 0)
+                              & (stock_datas["最低价"] != 0) & (stock_datas["开盘价"] != 0)
+                              & (stock_datas["成交量"] != 0)]
+    # 修改列
+    stock_datas.rename(
+        columns={'日期': 'date', '收盘价': 'close', '最高价': 'high', '最低价': 'low', '开盘价': 'open', '成交量': 'volume'}
+        , inplace=True)
+    # 将数据倒序输出
+    stock_datas = stock_datas.reindex(index=stock_datas.index[::-1])
+    stock = StockDataFrame.retype(stock_datas)
+    # sma
+    stock['close_5_sma'], stock['close_10_sma'], stock['close_30_sma'], stock['close_60_sma']
+    # ema
+    # boll:boll-中线  boll_ub:boll-up上线  boll_lb: boll下线
+    stock['boll'], stock['boll_ub'], stock['boll_lb']
+    # macd:DIF macds:DEA macdh*2:MACD
+    stock['macd'], stock['macds'], stock['macdh']
+    # kdj
+    stock['kdjk'], stock['kdjd'], stock['kdjj']
+    # rsi
+    stock['rsi_6'], stock['rsi_12'], stock['rsi_24']
+
+    stock.to_csv("D:\\1111.csv", encoding="GBK")
